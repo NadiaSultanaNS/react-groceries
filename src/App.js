722edf4a690602e442a3c5a96ff8866b8099b1df
Nadia Sultana <nadiasultana8899@gmail.com>
2022-01-14 17:54:25 -0500

    react groceries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      groceries: [
+        {
+          item: 'Potato',
+          brand: 'HelloPotato',
+          units: '2 lbs',
+          quantity: 5,
+          isPurchased: false
+        },
+        {
+          item: 'Chicken',
+          brand: 'HelloChicken',
+          units: '3 lbs',
+          quantity: 3,
+          isPurchased: false
+        },
+        {
+          item: 'Cheese',
+          brand: 'HelloCheese',
+          units: '1 Pack',
+          quantity: 4,
+          isPurchased: false
+        }
+      ],
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 }
 
 export default App;
